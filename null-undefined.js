101eab6b5511fc560f351d8852991fa3ff29abe6
MainuddinMazumder <mainuddinmazumder132@gmail.com>
2021-06-16 12:38:56 +0600

    null vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,21 @@
+when i will get null undefined value
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name:"smart dude", phone:11455};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 6];
+console.log(ages[11]);
