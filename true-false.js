50ed888d88a4f821aaf26395eaf650284f965f12
MainuddinMazumder <mainuddinmazumder132@gmail.com>
2021-06-16 11:52:01 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+//falsy: this values are initially false 
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+//Truthy: this values are truth 
+//" "
+//"0"
+//[]
+//{}
+//any type of number
+
+const age = {};
+
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
